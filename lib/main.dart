52325202b660728d16e37ee2748843737c120f70
KevMi <kevin.miederer@celpear.com>
2022-12-15 19:28:17 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'module/flutdot.dart';
 import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:ffi';
+
+import 'dart:io'
+    show
+        HttpRequest,
+        HttpServer,
+        InternetAddress,
+        WebSocket,
+        WebSocketTransformer;
+import 'dart:convert' show json;
+import 'dart:async' show Timer;
 
 late FlutDot FlutDotHandler;
 
@@ -76,4 +87,63 @@ class _HomePageState extends State<HomePage> {
       ),
     ));
   }
+}
+
+void initServer() {
+  HttpServer.bind('localhost', 8000).then((HttpServer server) {
+    print('[+]WebSocket listening at -- ws://localhost:8000/');
+    server.listen((HttpRequest request) {
+      WebSocketTransformer.upgrade(request).then((WebSocket ws) {
+        ws.listen(
+          (data) {
+            print(
+                '\t\t FluDot:${request?.connectionInfo?.remoteAddress} -- ${Map<String, String>.from(json.decode(data))}');
+
+            Timer(Duration(seconds: 1), () {
+              if (ws.readyState == WebSocket.open)
+                // checking connection state helps to avoid unprecedented errors
+                ws.add(json.encode({
+                  'data': 'from server at ${DateTime.now().toString()}',
+                }));
+            });
+          },
+          onDone: () => print('[+]Done :)'),
+          onError: (err) => print('[!]Error -- ${err.toString()}'),
+          cancelOnError: true,
+        );
+      }, onError: (err) => print('[!]Error -- ${err.toString()}'));
+    }, onError: (err) => print('[!]Error -- ${err.toString()}'));
+  }, onError: (err) => print('[!]Error -- ${err.toString()}'));
+}
+
+void initClient() {
+  WebSocket.connect('ws://localhost:8000').then((WebSocket ws) {
+    // our websocket server runs on ws://localhost:8000
+    if (ws?.readyState == WebSocket.open) {
+      // as soon as websocket is connected and ready for use, we can start talking to other end
+      ws.add(json.encode({
+        'data': 'from client at ${DateTime.now().toString()}',
+      })); // this is the JSON data format to be transmitted
+      ws.listen(
+        // gives a StreamSubscription
+        (data) {
+          print(
+              '\t\t -- ${Map<String, String>.from(json.decode(data))}'); // listen for incoming data and show when it arrives
+          Timer(Duration(seconds: 1), () {
+            if (ws.readyState ==
+                WebSocket
+                    .open) // checking whether connection is open or not, is required before writing anything on socket
+              ws.add(json.encode({
+                'data': 'from client at ${DateTime.now().toString()}',
+              }));
+          });
+        },
+        onDone: () => print('[+]Done :)'),
+        onError: (err) => print('[!]Error -- ${err.toString()}'),
+        cancelOnError: true,
+      );
+    } else
+      print('[!]Connection Denied');
+    // in case, if serer is not running now
+  }, onError: (err) => print('[!]Error -- ${err.toString()}'));
 }
